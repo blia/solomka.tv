58e7925712f3088192a5f608f5bd680caa790acd
Kirill Yakovenko <kirill.yakovenko@me.com>
2011-07-31 12:02:37 +0300

    some js simple code

--- app.js
+++ app.js
@@ -52,31 +52,31 @@ app.get('/chat', function(req, res){
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-var io = require('socket.io').listen(app);
-
-var nicknames = {};
-
-io.sockets.on('connection', function (socket) {
-  socket.on('user message', function (msg) {
-    socket.broadcast.emit('user message', socket.nickname, msg);
-  });
-
-  socket.on('nickname', function (nick, fn) {
-    if (nicknames[nick]) {
-      fn(true);
-    } else {
-      fn(false);
-      nicknames[nick] = socket.nickname = nick;
-      socket.broadcast.emit('announcement', nick + ' connected');
-      io.sockets.emit('nicknames', nicknames);
-    }
-  });
-
-  socket.on('disconnect', function () {
-    if (!socket.nickname) return;
-
-    delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
-  });
-});
+// var io = require('socket.io').listen(app);
+// 
+// var nicknames = {};
+// 
+// io.sockets.on('connection', function (socket) {
+//   socket.on('user message', function (msg) {
+//     socket.broadcast.emit('user message', socket.nickname, msg);
+//   });
+// 
+//   socket.on('nickname', function (nick, fn) {
+//     if (nicknames[nick]) {
+//       fn(true);
+//     } else {
+//       fn(false);
+//       nicknames[nick] = socket.nickname = nick;
+//       socket.broadcast.emit('announcement', nick + ' connected');
+//       io.sockets.emit('nicknames', nicknames);
+//     }
+//   });
+// 
+//   socket.on('disconnect', function () {
+//     if (!socket.nickname) return;
+// 
+//     delete nicknames[socket.nickname];
+//     socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+//     socket.broadcast.emit('nicknames', nicknames);
+//   });
+// });
